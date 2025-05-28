8cf21b8b40c17491a1023f98bbef10f16c2c7ff6
elnatnael <meranat9104@gmail.com>
2025-05-28 13:17:46 +0300

    complex queries to retrieve data

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -35,7 +35,10 @@ SELECT
 FROM
     property
 LEFT JOIN
-    review ON property.property_id = review.property_id;
+    review ON property.property_id = review.property_id
+ORDER BY
+    property.name ASC, review.rating DESC;
+
 -- A query  using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user:
 SELECT
     u.user_id,

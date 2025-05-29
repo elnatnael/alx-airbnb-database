2d0cd6b227009d4b5162f91f22d25aa932ad4390
elnatnael <meranat9104@gmail.com>
2025-05-29 06:11:48 +0300

    aggrigation

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,13 @@
+
+----Apply Aggregations and Window Functions:
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    property p
+LEFT JOIN 
+    booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name;

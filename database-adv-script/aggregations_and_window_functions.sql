637d1904453a7930878ff5a515fda3f386f32347
elnatnael <meranat9104@gmail.com>
2025-05-29 06:24:28 +0300

    aggrigation

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,14 +1,31 @@
 
-----Apply Aggregations and Window Functions:
+----Apply Aggregations 
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    user u
+LEFT JOIN 
+    booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name;
+
+
+----- Windows function
+
 SELECT 
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
 FROM 
     property p
 LEFT JOIN 
     booking b ON p.property_id = b.property_id
 GROUP BY 
     p.property_id, p.name;
+

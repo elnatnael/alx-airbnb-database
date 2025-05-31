b55ac6df2ed9c3ae1279a0df37935e064c17b2bb
elnatnael <meranat9104@gmail.com>
2025-05-31 07:00:03 +0300

    optimaization using indexes

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,4 @@
--- User Table Indexes
+uii-- User Table Indexes
 CREATE UNIQUE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_user_role ON User(role);
 
@@ -16,7 +16,7 @@ CREATE INDEX idx_property_pricepernight ON Property(pricepernight);
 -- Checks for Measure the query performance
     before adding indexes:
        -- Booking queries
-EXPLAIN SELECT * FROM Booking WHERE user_id = 'some-user-id' ORDER BY start_date;
+ANALYZE SELECT * FROM Booking WHERE user_id = 'some-user-id' ORDER BY start_date;
 EXPLAIN SELECT * FROM Booking WHERE property_id = 'some-property-id' AND status = 'confirmed';
 
 -- Property queries
@@ -30,15 +30,15 @@ EXPLAIN SELECT * FROM User WHERE role = 'guest' ORDER BY created_at;
    After adding indexes:
 
 -- User Table
-EXPLAIN SELECT * FROM User WHERE email = 'user@example.com';
-EXPLAIN SELECT * FROM User WHERE role = 'guest' ORDER BY created_at;
+ANALYZE SELECT * FROM User WHERE email = 'user@example.com';
+ANALYZE SELECT * FROM User WHERE role = 'guest' ORDER BY created_at;
 
 -- Booking Table
-EXPLAIN SELECT * FROM Booking WHERE user_id = 'some-user-id' ORDER BY created_at;
-EXPLAIN SELECT * FROM Booking WHERE property_id = 'some-property-id' AND status = 'confirmed';
+ANALYZE SELECT * FROM Booking WHERE user_id = 'some-user-id' ORDER BY created_at;
+ANALYZE SELECT * FROM Booking WHERE property_id = 'some-property-id' AND status = 'confirmed';
 EXPLAIN SELECT * FROM Booking WHERE start_date >= '2025-01-01' AND end_date <= '2025-01-10';
 
 -- Property Table
-EXPLAIN SELECT * FROM Property WHERE location = 'New York' ORDER BY pricepernight;
+ANALYZE SELECT * FROM Property WHERE location = 'New York' ORDER BY pricepernight;
 EXPLAIN SELECT * FROM Property WHERE host_id = 'some-host-id' ORDER BY created_at;
 
